0cd15c7274da04d1972e5c0bf96dcb4608a9bd8b
Arif <arif.rahman@wcma.vic.gov.au>
2025-05-21 13:26:15 +1000

    done

--- convert.py
+++ convert.py
@@ -1,10 +1,5 @@
 import arcpy
-import os
 
-mxd_path = r"C:\Files\MXDs\20250520_VLP_Boundaries.mxd"
-template_aprx = r"C:\Files\blank.aprx"
-output_aprx = r"C:\Files\20250520_VLP_Boundaries.aprx"
-
-aprx = arcpy.mp.ArcGISProject(template_aprx)
-aprx.importDocument(mxd_path)
-aprx.saveACopy(output_aprx)
+aprx = arcpy.mp.ArcGISProject(r"C:\Files\blank.aprx")
+aprx.importDocument(r"C:\Files\20250520_VLP_Boundaries.mxd")
+aprx.saveACopy(r"C:\Files\20250520_VLP_Boundaries.aprx")

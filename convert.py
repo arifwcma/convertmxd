d27253bcfb14277613558738473ba4f62e3ee852
Arif <arif.rahman@wcma.vic.gov.au>
2025-07-11 10:33:58 +1000

    done

--- convert.py
+++ convert.py
@@ -4,7 +4,6 @@ import os
 print("Start")
 aprx = arcpy.mp.ArcGISProject(r"C:\Files\mig\blank.aprx")
 aprx.importDocument(r"C:\files\mig\Original\A0_landscapeA.mxd")
-os.makedirs(r"C:\files\mig\Original\A0_landscapeA", exist_ok=True)
-dest = r"C:\files\mig\Original\A0_landscapeA\A0_landscapeA.aprx"
-aprx.saveACopy(dest)
+os.makedirs(r"C:\files\mig\Converted\A0_landscapeA", exist_ok=True)
+aprx.saveACopy(r"C:\files\mig\Converted\A0_landscapeA\A0_landscapeA.aprx")
 print("End")

61a7c2ac015870926b2a995dcdb8f592d91b6e4b
Arif <arif.rahman@wcma.vic.gov.au>
2025-07-11 10:29:56 +1000

    done

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 import arcpy
+import os
 
-print("Hi")
-
+print("Start")
 aprx = arcpy.mp.ArcGISProject(r"C:\Files\mig\blank.aprx")
-aprx.importDocument(r"C:\Files\old\Bryana\IPAWS_A5.mxd")
-#sde_path = r"C:\Files\old\MXDs\blank\PostgreSQL-gisap01-sdc(gisuser)2.sde"
-dest = r"C:\Files\mig\dest2.aprx"
+aprx.importDocument(r"C:\files\mig\Original\A0_landscapeA.mxd")
+os.makedirs(r"C:\files\mig\Original\A0_landscapeA", exist_ok=True)
+dest = r"C:\files\mig\Original\A0_landscapeA\A0_landscapeA.aprx"
 aprx.saveACopy(dest)
-
+print("End")

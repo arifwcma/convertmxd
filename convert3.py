650984b9bea43cef717941f2acf44225c9c251e0
arifwcma <arif.rahman@wcma.vic.gov.au>
2026-03-11 15:49:18 +1100

    done

--- convert3.py
+++ convert3.py
@@ -10,7 +10,7 @@ for file in os.listdir(src):
     if file.endswith(".mxd"):
         src_mxd = os.path.join(src, file)
         file_wo_mxd = os.path.splitext(file)[0]
-        d = os.path.join(dest, file)
+        d = os.path.join(dest, file_wo_mxd)
         shutil.copytree(blank_folder, d, dirs_exist_ok=True)
         blank_aprx = os.path.join(d, "blank.aprx")
         final_aprx = os.path.join(d, file_wo_mxd + ".aprx")
